4e31a29b5f875257d138c1dae7f06e52cf3e094f
Feng_Noti <Feng_14@outlook.com>
2025-12-28 22:03:58 +0800

    1
    1

--- 厅内.py
+++ 厅内.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 # Comb Mosha 2025。
 from random import *
-from re import M
 from rich.progress import *
 from rich.console import Console
 import os
 import time
 import sys
 import math
-import webbrowser as wb
 import keyboard as kb
 cs = Console()
 
@@ -30,22 +28,45 @@ color = {
     "text": "#ffffff",
 }
 
-z_name = [f"Z - {i}" for i in range(10)] # 角色名称。
-zj_atk = [randint(1, 14) for i in range(10)] # 角色攻击力。
-zj_crit = [uniform(0.1, 0.9) for i in range(10)] # 角色暴击率。
-zj_fy = [randint(1, 14) for i in range(10)] # 角色防御力。
-zj_jc = [randint(9, 17) for i in range(10)] # 角色 JC。
+z_x = []
+z_y = []
 
-d_name = [f"D - {i}" for i in range(10)] # 敌人名称。
-d_atk = [randint(1, 14) for i in range(10)] # 敌人攻击力。
-d_crit = [uniform(0.1, 0.9) for i in range(10)] # 敌人暴击率。
-d_fy = [randint(1, 14) for i in range(10)] # 敌人防御力。
-d_jc = [randint(9, 17) for i in range(10)] # 敌人 JC。
+d_x = []
+d_y = []
 
-pc_string = ""
+class Z: # 角色属性。
+    def __init__(self, hp, thp, name, atk, crit, fy, jc, exist, zdz):
+        self.hp = hp # 角色 HP。
+        self.thp = thp # 角色总 HP。
+        self.name = name # 角色名称。
+        self.atk = atk # 角色攻击力。
+        self.crit = crit # 角色暴击率。
+        self.fy = fy # 角色防御力。
+        self.jc = jc # 角色 JC。
+        self.exist = exist # 角色是否存活。
+        self.zdz = zdz # 角色在与谁战斗？
 
-turns = 0
-police_join = False
+class D: # 敌人属性。
+    def __init__(self, hp, thp, name, atk, crit, fy, jc, exist, zdz):
+        self.hp = hp # 敌人 HP。
+        self.thp = thp # 敌人总 HP。
+        self.name = name # 敌人名称。
+        self.atk = atk # 敌人攻击力。
+        self.crit = crit # 敌人暴击率。
+        self.fy = fy # 敌人防御力。
+        self.jc = jc # 敌人 JC。
+        self.exist = exist # 敌人是否存活。
+        self.zdz = zdz # 敌人在与谁战斗？
+
+z_sx = [
+    Z(0, 0, f"Z - {i}", randint(1, 14), uniform(0.1, 0.9), randint(1, 14), randint(9, 17), True, -1)
+    for i in range(10)
+]
+
+d_sx = [
+    D(0, 0, f"D - {i}", randint(1, 14), uniform(0.1, 0.9), randint(1, 14), randint(9, 17), True, -1)
+    for i in range(10)
+]
 
 try:
     def zf(text, cl):
@@ -206,71 +227,79 @@ try:
         z_check = 0
         d_check = 0
 
-        z_check = zj_jc[z_ord] * 3.306 - zj_fy[z_ord]
-        d_check = d_jc[d_ord] * 3.306 - d_fy[d_ord]
+        z_check = z_sx[z_ord].jc * 3.306 - z_sx[z_ord].fy
+        d_check = d_sx[d_ord].jc * 3.306 - d_sx[d_ord].fy
 
+        print("初始状态。")
+        jdt(z_sx[z_ord].hp, z_sx[z_ord].thp, f"{z_sx[z_ord].name}", "hp", "me")
+        jdt(d_sx[d_ord].hp, d_sx[d_ord].thp, f"{d_sx[d_ord].name}", "hp", "enemy")
+        input()
+
+        z_damage = randint(6, 9) + z_sx[z_ord].atk
+        z_acc = mz(z_sx[z_ord].name, d_sx[d_ord].name)
         print()
-        z_damage = randint(6, 9) + zj_atk[z_ord]
-        z_acc = mz(z_name[z_ord], d_name[d_ord])
-        if 4 <= z_acc <= 6:
-            zf(f"{z_name[z_ord]} 打出了精准的一招。", "乙")
-            z_damage *= (1 + zj_crit[z_ord] / 10)
+        if 24 <= z_acc <= 25:
+            zf(f"{z_sx[z_ord].name} 打出了精准的一招。", "乙")
+            z_damage *= (1 + (z_sx[z_ord].crit - abs(5 - z_acc) / 45) / 10)
+        elif z_acc == 0:
+            zf(f"{z_sx[z_ord].name} 落空了。", "癸")
+            z_damage = 0
         else:
-            zf(f"{z_name[z_ord]} 未能精准命中。", "壬")
-        os.system("cls")
-
-        d_damage = randint(6, 9) + d_atk[d_ord]
-        d_acc = mz(d_name[d_ord], z_name[z_ord])
-        if 4 <= d_acc <= 6:
-            zf(f"{d_name[d_ord]} 打出了精准的一招。", "壬")
-            d_damage*= (1 + d_crit[d_ord] / 10)
-        else:
-            zf(f"{d_name[d_ord]} 未能精准命中。", "乙")
-        os.system("cls")
-
-        # 计算实际造成的伤害。
+            zf(f"{z_sx[z_ord].name} 未能精准命中。", "壬")
+            z_damage *= (1 - abs(5 - z_acc) / 25)
+        print()
         if d_check <= 0:
             z_damage = 0
         z_damage *= (d_check / 10) * uniform(0.95, 1.05)
-        zf(f"{z_name[z_ord]} 对 {d_name[d_ord]} 造成了 {max(abs(z_damage), 0):.3f} HP 伤害。", "text")
-        ds_hp[d_ord] -= z_damage
+        zf(f"{z_sx[z_ord].name} 对 {d_sx[d_ord].name} 造成了 {max(abs(z_damage), 0):.3f} HP 伤害。", "text")
+        d_sx[d_ord].hp -= z_damage
 
+        d_damage = randint(6, 9) + d_sx[d_ord].atk
+        d_acc = mz(d_sx[d_ord].name, z_sx[z_ord].name)
+        print()
+        if 24 <= d_acc <= 25:
+            zf(f"{d_sx[d_ord].name} 打出了精准的一招。", "壬")
+            d_damage*= (1 + (d_sx[d_ord].atk - abs(5 - d_acc) / 45) / 10)
+        elif d_acc == 0:
+            zf(f"{d_sx[d_ord].name} 落空了。", "甲")
+            d_damage = 0
+        else:
+            zf(f"{d_sx[d_ord].name} 未能精准命中。", "乙")
+            d_damage *= (1 - abs(5 - d_acc) / 25)
         print()
         if z_check <= 0:
             d_damage = 0
         d_damage *= (z_check / 10) * uniform(0.95, 1.05)
-        zf(f"{d_name[d_ord]} 对 {z_name[z_ord]} 造成了 {max(abs(d_damage), 0):.3f} HP 伤害。", "text")
-        zs_hp[z_ord] -= d_damage
+        zf(f"{d_sx[d_ord].name} 对 {z_sx[z_ord].name} 造成了 {max(abs(d_damage), 0):.3f} HP 伤害。", "text")
+        z_sx[z_ord].hp -= d_damage
 
+        os.system("cls")
         print()
         # 检查敌人状态。
-        if ds_hp[d_ord] <= 0:
-            zf(f"{d_name[d_ord]} 败下阵来。", "甲")
+        if d_sx[d_ord].hp <= 0:
+            zf(f"{d_sx[d_ord].name} 败下阵来。", "甲")
             monitor_killed += 1
-            d_exist[d_ord] = False
-            d_infight[d_ord] = False
-            d_fighter[d_ord] = -1
-            z_infight[z_ord] = False
-            z_fighter[z_ord] = -1
+            d_sx[d_ord].exist = False
+            d_sx[d_ord].zdz = -1
+            z_sx[z_ord].zdz = -1
             maze[d_x[d_ord]][d_y[d_ord]] = f"| . |"
             maze[z_x[z_ord]][z_y[z_ord]] = f"| Z - {z_ord} |"
             return
 
         # 检查角色状态。
-        if zs_hp[z_ord] <= 0:
-            zf(f"{z_name[z_ord]} 败下阵来。", "癸")
+        if z_sx[z_ord].hp <= 0:
+            zf(f"{z_sx[z_ord].name} 败下阵来。", "癸")
             player_caught += 1
-            z_exist[z_ord] = False
-            d_infight[d_ord] = False
-            d_fighter[d_ord] = -1
-            z_infight[z_ord] = False
-            z_fighter[z_ord] = -1
+            z_sx[z_ord].exist = False
+            d_sx[d_ord].zdz = -1
+            z_sx[z_ord].zdz = -1
             maze[z_x[z_ord]][z_y[z_ord]] = f"| . |"
             maze[d_x[d_ord]][d_y[d_ord]] = f"| D - {d_ord} |"
             return
 
-        jdt(zs_hp[z_ord], zt_hp[z_ord], f"{z_name[z_ord]}", "hp", "me")
-        jdt(ds_hp[d_ord], dt_hp[d_ord], f"{d_name[d_ord]}", "hp", "enemy")
+        print("结束状态。")
+        jdt(z_sx[z_ord].hp, z_sx[z_ord].thp, f"{z_sx[z_ord].name}", "hp", "me")
+        jdt(d_sx[d_ord].hp, d_sx[d_ord].thp, f"{d_sx[d_ord].name}", "hp", "enemy")
         os.system("pause")
         print()
 
@@ -316,29 +345,45 @@ try:
    //   ||   \\                            //   ||   \\
         """)
 
-        ls_string = list("....................")
+        ls_string = list("........................**........................")
         ls_range = len(ls_string)
 
         sys.stdout.write("".join(ls_string))
         sys.stdout.flush()
 
-        l = 0
 
-        for i in range(ls_range):
-            if l > 0:
-                ls_string[l - 1] = "."
-            ls_string[l] = "|"
+        if randint(0, 1):
+            l = 0
+            for i in range(ls_range):
+                if l > 0:
+                    ls_string[l - 1] = "."
+                ls_string[l] = "|"
 
-            sys.stdout.write("\r" + "".join(ls_string))
-            sys.stdout.flush()
+                sys.stdout.write("\r" + "".join(ls_string))
+                sys.stdout.flush()
 
-            l = (l + 1) % ls_range
-            time.sleep(0.05)
+                l = (l + 1) % ls_range
+                time.sleep(0.025)
 
-            if kb.is_pressed("z"):
-                break
+                if kb.is_pressed("z"):
+                    break
+        else:
+            l = ls_range - 1
+            for j in range(ls_range - 1, -1, -1):
+                if l < ls_range - 1:
+                    ls_string[l + 1] = "."
+                ls_string[l] = "|"
 
-        return ceil(l / 2)
+                sys.stdout.write("\r" + "".join(ls_string))
+                sys.stdout.flush()
+
+                l = (l - 1) % ls_range
+                time.sleep(0.025)
+
+                if kb.is_pressed("z"):
+                    break
+
+        return l
 except EOFError as e:
     print("发生了 EOF 错误。你可能按下了 Ctrl + Z 组合键。")
     sys.exit(0)
@@ -362,35 +407,27 @@ d_amount = zs(zf("看守者数量？", "inp"), 1, 9)
 
 print()
 zf("…………", "text")
-z_x = []
-z_y = []
-zt_hp = [] # 角色总 HP。
-dt_hp = [] # 敌人总 HP。
 
 for i in range(z_amount):
     if z_amount == 1:
         z_x.append(zs(zf("角色起始纵坐标？（X 从 0 开始）", "inp"), 0, 9))
         z_y.append(zs(zf("角色起始横坐标？（Y 从 0 开始）", "inp"), 0, 9))
-        zt_hp.append(zs(zf("角色起始 HP？", "inp"), 1, float("inf")))
+        z_sx[i].thp = zs(zf("角色起始 HP？", "inp"), 1, float("inf"))
     else:
         z_x.append(zs(zf(f"第 {i + 1} 位角色的起始纵坐标？（X 从 0 开始）", "inp"), 0, 9))
         z_y.append(zs(zf(f"第 {i + 1} 位角色的起始横坐标？（Y 从 0 开始）", "inp"), 0, 9))
-        zt_hp.append(zs(zf(f"第 {i + 1} 位角色的起始 HP？", "inp"), 1, float("inf")))
+        z_sx[i].thp = zs(zf(f"第 {i + 1} 位角色的起始 HP？", "inp"), 1, float("inf"))
+    z_sx[i].hp = z_sx[i].thp        
 
 zf("…………", "text")
 for j in range(d_amount):
     if d_amount == 1:
-        dt_hp.append(zs(zf("看守者起始 HP？", "inp"), 1, float("inf")))
+        d_sx[j].thp = zs(zf("看守者起始 HP？", "inp"), 1, float("inf"))
     else:
-        dt_hp.append(zs(zf(f"第 {j + 1} 位看守者的起始 HP？", "inp"), 1, float("inf")))
-
-zs_hp = list(zt_hp) # 角色 HP。
-ds_hp = dt_hp[:] # 敌人 HP。
+        d_sx[j].thp = zs(zf(f"第 {j + 1} 位看守者的起始 HP？", "inp"), 1, float("inf"))
+    d_sx[j].hp = d_sx[j].thp
 
 os.system("cls")
-
-d_x = []
-d_y = []
 
 maze = [
     ["| . |", "| . |", "| . |", "| . |", "| . |", "| . |", "| . |", "| . |", "| . |", "| . |"],
@@ -421,12 +458,6 @@ for ls_1 in range(d_amount): # 随机分配监视者的位置
 player_caught = 0
 monitor_killed = 0
 player_step = 0
-z_exist = [True for i in range(z_amount)]
-d_exist = [True for i in range(d_amount)]
-z_infight = [False for i in range(z_amount)]
-d_infight = [False for i in range(d_amount)]
-z_fighter = [-1 for i in range(z_amount)]
-d_fighter = [-1 for i in range(d_amount)]
 
 def z_move(num):
     global player_caught, player_step
@@ -437,7 +468,7 @@ def z_move(num):
     使用方向键控制角色，按 ESC 暂停，按 F1 跳过本回合。""")
     print(f"""{f'第 {player_step} 步，共 {limit_steps} 步，还有 {limit_steps - player_step} 步要走' if limit_steps > player_step else '这是最后一步'}。
     {f'已有 {player_caught} 名角色被捕' if player_caught > 0 else '无角色被捕'}，{f'已有 {monitor_killed} 名监视者出局' if monitor_killed > 0 else '无看守者出局'}。""")
-    print()
+
     for i in range(10):
         for j in range(10):
             print(maze[i][j], end=" ")
@@ -452,10 +483,8 @@ def z_move(num):
                 if "D" in maze[z_x[num] - 1][z_y[num]]:
                     ls_dord = maze[z_x[num] - 1][z_y[num]].replace("|", "").replace(" ", "").replace("D-", "")
                     zf(f"Z - {num} 被 D - {ls_dord} 捕获。", "癸")
-                    z_infight[num] = True
-                    z_fighter[num] = int(ls_dord)
-                    d_infight[int(ls_dord)] = True
-                    d_fighter[int(ls_dord)] = num
+                    z_sx[num].zdz = int(ls_dord)
+                    d_sx[int(ls_dord)].zdz = num
                     return
                 else:
                     maze[z_x[num]][z_y[num]] = "| . |"
@@ -469,10 +498,8 @@ def z_move(num):
                 if "D" in maze[z_x[num] + 1][z_y[num]]:
                     ls_dord = maze[z_x[num] + 1][z_y[num]].replace("|", "").replace(" ", "").replace("D-", "")
                     zf(f"Z - {num} 被 D - {ls_dord} 捕获。", "癸")
-                    z_infight[num] = True
-                    z_fighter[num] = int(ls_dord)
-                    d_infight[int(ls_dord)] = True
-                    d_fighter[int(ls_dord)] = num
+                    z_sx[num].zdz = int(ls_dord)
+                    d_sx[int(ls_dord)].zdz = num
                     return
                 else:
                     maze[z_x[num]][z_y[num]] = "| . |"
@@ -486,10 +513,8 @@ def z_move(num):
                 if "D" in maze[z_x[num]][z_y[num] - 1]:
                     ls_dord = maze[z_x[num]][z_y[num] - 1].replace("|", "").replace(" ", "").replace("D-", "")
                     zf(f"Z - {num} 被 D - {ls_dord} 捕获。", "癸")
-                    z_infight[num] = True
-                    z_fighter[num] = int(ls_dord)
-                    d_infight[int(ls_dord)] = True
-                    d_fighter[int(ls_dord)] = num
+                    z_sx[num].zdz = int(ls_dord)
+                    d_sx[int(ls_dord)].zdz = num
                     return
                 else:
                     maze[z_x[num]][z_y[num]] = "| . |"
@@ -503,10 +528,8 @@ def z_move(num):
                 if "D" in maze[z_x[num]][z_y[num] + 1]:
                     ls_dord = maze[z_x[num]][z_y[num] + 1].replace("|", "").replace(" ", "").replace("D-", "")
                     zf(f"Z - {num} 被 D - {ls_dord} 捕获。", "癸")
-                    z_infight[num] = True
-                    z_fighter[num] = int(ls_dord)
-                    d_infight[int(ls_dord)] = True
-                    d_fighter[int(ls_dord)] = num
+                    z_sx[num].zdz = int(ls_dord)
+                    d_sx[int(ls_dord)].zdz = num
                     return
                 else:
                     maze[z_x[num]][z_y[num]] = "| . |"
@@ -527,8 +550,10 @@ def z_move(num):
             break
 
 def d_move(num):
+    os.system("cls")
     global player_caught
     x_change = True
+    ls_caughtzf = ""
 
     while True:
         ls_cx = randint(-1, 1)
@@ -548,38 +573,44 @@ def d_move(num):
                 inf_y = f"{'也' if x_change else '却'}向右移动了 {abs(ls_cy)} 格"
             else:
                 inf_y = f"{'也' if x_change else '却'}向左移动了 {abs(ls_cy)} 格"
-            zf(f"D - {num} {inf_x}，{inf_y}。", "text")
             
             if "Z" in maze[d_x[num] + ls_cx][d_y[num] + ls_cy]:
                 ls_zord = maze[d_x[num] + ls_cx][d_y[num] + ls_cy].replace("|", "").replace(" ", "").replace("Z-", "")
-                zf(f"D - {num} 将 Z - {ls_zord} 捕获。", "癸")
-                z_infight[int(ls_zord)] = True
-                z_fighter[int(ls_zord)] = num
-                d_infight[num] = True
-                d_fighter[num] = int(ls_zord)
+                ls_caughtzf = f"D - {num} 将 Z - {ls_zord} 捕获。"
+                z_sx[int(ls_zord)].zdz = num
+                d_sx[num].zdz = int(ls_zord)
             maze[d_x[num]][d_y[num]] = "| . |"
             d_x[num] += ls_cx
             d_y[num] += ls_cy
             maze[d_x[num]][d_y[num]] = f"| D - {num} |"
+            
+            print("看守者移动。")
+            print()
+            for i in range(10):
+                for j in range(10):
+                    print(maze[i][j], end=" ")
+                print()
+
+            print()
+            zf(f"D - {num} {inf_x}，{inf_y}。", "text")
+            if len(ls_caughtzf) > 0:
+                zf(ls_caughtzf, "癸")
             break
 
 while player_caught < z_amount and monitor_killed < d_amount:
     for a in range(z_amount):
-        if z_exist[a] and not z_infight[a]:
+        if z_sx[a].exist and z_sx[a].zdz == -1:
             z_move(a)
-        elif z_infight[a]:
+        elif z_sx[a].zdz != -1:
             os.system("cls")
-            gj(a, z_fighter[a])
-    
-    os.system("cls")
+            gj(a, z_sx[a].zdz)
 
     for b in range(d_amount):
-        if d_exist[b] and not d_infight[b]:
+        if d_sx[b].exist and d_sx[b].zdz == -1:
             d_move(b)
-        elif d_infight[b]:
+        elif d_sx[b].zdz != -1:
             os.system("cls")
-            gj(d_fighter[b], b)
-    print()
+            gj(d_sx[b].zdz, b)
 
     if player_step == limit_steps or monitor_killed == d_amount:
         zf("你赢了！", "甲")
